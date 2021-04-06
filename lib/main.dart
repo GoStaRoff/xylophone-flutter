467049212eb9b4cd4c82ae0ce60f7ff47c74ef66
Oleksandr <gostar1337@gmail.com>
2021-04-06 20:26:48 +0300

    Section Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  final AudioCache player = new AudioCache();
+
+  void playSound(int soundIndex) {
+    player.play('note$soundIndex.wav');
+  }
+
+  Expanded buildKey(int soundIndex, MaterialColor color) {
+    return Expanded(
+      child: Container(
+        color: color,
+        child: FlatButton(
+          onPressed: () {
+            playSound(soundIndex);
+          },
+          child: Text(''),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
         ),
       ),
     );
